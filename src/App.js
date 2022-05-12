1e8d1255d9a9437f211885ef1dfee3e589ffc920
Soyoung Kim <sykim15@cafe24corp.com>
2022-05-12 14:35:15 +0900

    feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import './App.css';
+import { sliderData } from "./slide-data";
 
 function App() {
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const slideLength = sliderData.length;
+  const auto = false;
+  let intervalTime = 5000;
+
+ const nextSlide = useCallback(() => {
+    setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+
+    console.log("next");
+  },[currentSlide, slideLength]);
+
+  const prevSlide = useCallback(() => {
+    setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+    console.log("prev");
+  },[currentSlide, slideLength]);
+
+
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+  
+    useEffect(() => {
+      savedCallback.current = callback;
+    });
+  
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();//callback
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+  useInterval(()=>{
+    if (currentSlide === sliderData.length - 1) {
+      setCurrentSlide(0);
+    } else {
+      nextSlide();
+    }
+  },  auto ? intervalTime : null)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="slider">
+      <div className="arrow prev" onClick={prevSlide} >prevSlide</div>
+      <div className="arrow next" onClick={nextSlide}>nextSlide</div>
+      {sliderData.map((slide, index) => {
+        return (
+          <div
+            className={index === currentSlide ? "slide current" : "slide"}
+            key={index}
+          >
+            <div>
+                <div className="content">
+                  <h2>{slide.heading}</h2>
+                  <p>{slide.desc}</p>
+                  <div className={slide.image}></div>
+                  <button className="--btn --btn-primary">Get Started</button>
+                </div>
+              </div>
+          </div>
+        );
+      })}
     </div>
   );
+
+
 }
 
 export default App;
---
+++ src/slide-data.js
@@ -0,0 +1,22 @@
+export const sliderData = [
+    {
+      image: "slide_01",
+      heading: "Slide One",
+      desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: "slide_02",
+      heading: "Slide Two",
+      desc: "This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: "slide_03",
+      heading: "Slide Three",
+      desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: "slide_04",
+      heading: "Slide Four",
+      desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+  ];
